2b15458da83feda7aee1adb088fad4e2b137d52b
Matthias Braeger <Matthias.Braeger@cern.ch>
2020-04-06 15:38:35 +0200

    CM-246: Refactor JmsProxyImpl to breakdown monolithic structure

--- c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/config/JmsConfig.java
+++ c2mon-client/c2mon-client-core/src/main/java/cern/c2mon/client/core/config/JmsConfig.java
@@ -16,6 +16,10 @@
  *****************************************************************************/
 package cern.c2mon.client.core.config;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.ActiveMQPrefetchPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,5 +62,24 @@ public class JmsConfig {
   @Bean
   public JmsTemplate clientJmsTemplate() {
     return new JmsTemplate(new CachingConnectionFactory(clientJmsConnectionFactory()));
+  }
+  
+  /**
+   * Threads used for polling topic queues.
+   * @return The topic polling Executor Service
+   */
+  @Bean
+  public ExecutorService topicPollingExecutor() {
+    return Executors.newCachedThreadPool(new ThreadFactory() {
+      
+      ThreadFactory defaultFactory = Executors.defaultThreadFactory();
+      
+      @Override
+      public Thread newThread(final Runnable r) {
+        Thread returnThread = defaultFactory.newThread(r);
+        returnThread.setDaemon(true);
+        return returnThread;
+      }
+    });
   }
 }
